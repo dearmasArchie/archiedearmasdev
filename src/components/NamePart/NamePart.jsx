d87a982f2b7194a38414c647caa4bca98ef7b3ce
archiedearmas.dev <dearmasarchie@gmail.com>
2025-12-10 21:30:49 +0800

    updated file

--- src/components/NamePart/NamePart.jsx
+++ src/components/NamePart/NamePart.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import './name-part.css';
-import formalPicture from '../../assets/formal-picture.jpg';
+import formalPicture from '../../assets/formal-picture.png';
 
 function NamePart() {
     return (
     <div className='side-space'>
-      <div className='left-side'>
-        <p>Hello I'm</p>
-        <h1 className='name'>ARCHIE DE ARMAS</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
+      <div className='parent'>
+        <div className='div2 left-side'>
+          <p>Hello I'm</p>
+          <h1 className='name'>ARCHIE DE ARMAS</h1> 
+        </div>
 
-      <div className='right-side'>
-        <img src={formalPicture} alt="Archie De Armas"/>
+        <div className='div1 right-side'>
+          <img src={formalPicture} alt="Archie De Armas"/>
+        </div>
+
+        <div className='div3 left-side description'> 
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
     </div>
     );
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
+// ...existing code...
+import React, { useState } from 'react';
 import './nav-bar.css';
 import archieveLogo from '../../assets/archie-logo.png';
 
 function NavBar() {
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => setOpen(prev => !prev);
+    const close = () => setOpen(false);
+
     return (
         <nav className='navbar'>
             <div className='myname'>
                 <img src={archieveLogo} alt="logo" />
             </div>
 
-            <div className='navigation'>
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#projects">Projects</a>
-                <a href="#contact">Contact</a>
+            <button
+                className={`hamburger ${open ? 'is-open' : ''}`}
+                aria-label="Toggle navigation"
+                aria-expanded={open}
+                onClick={toggle}
+            >
+                <span className="bar" />
+                <span className="bar" />
+                <span className="bar" />
+            </button>
+
+            <div className={`navigation ${open ? 'open' : ''}`}>
+                <a href="#home" onClick={close}>Home</a>
+                <a href="#about" onClick={close}>About</a>
+                <a href="#projects" onClick={close}>Projects</a>
+                <a href="#contact" onClick={close}>Contact</a>
             </div>
 
-            <div className='buttons'>
-                <a href="https://www.facebook.com/archie.dearmas" target='_blank'><button className='FaceBookbtn'>FaceBook</button></a>
-                <a href="https://www.instagram.com/archieqt3.14/" target='_blank'><button className='Instagrambtn'>Instagram</button></a>
+            <div className={`buttons ${open ? 'open' : ''}`}>
+                <a href="https://www.facebook.com/archie.dearmas" target='_blank' rel="noreferrer"><button className='FaceBookbtn' onClick={close}>FaceBook</button></a>
+                <a href="https://www.instagram.com/archieqt3.14/" target='_blank' rel="noreferrer"><button className='Instagrambtn' onClick={close}>Instagram</button></a>
             </div>
         </nav>
     );
-}   
+}
 
 export default NavBar;
+// ...existing code...
